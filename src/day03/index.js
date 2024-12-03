2a3d8c7abf61cec0c59f0b8e4865f9887a56ee30
wkgg <wky1991@gmail.com>
2024-12-03 21:24:03 +0800

    feat: finish 3-2

--- src/day03/index.js
+++ src/day03/index.js
@@ -17,8 +17,17 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  const newInput = input.replaceAll('\n', '').replace(/don't\(\).*?do\(\)/g, '').replace(/don't\(\).*$/, '');
+  console.log('newInput2: ', newInput);
+  const reg1 = /mul\(\d{1,3},\d{1,3}\)/g;
+  const corrupted = newInput.match(reg1);
+  let sum = 0;
+  const reg2 = /\d{1,3}/g;
+  for (const item of corrupted) {
+    const numbers = item.match(reg2);
+    sum += numbers[0] * numbers[1];
+  }
+  return sum;
 };
 
 run({
