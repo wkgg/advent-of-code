16773936e4002438298725326d2d52d1c8037b30
wkgg <wky1991@gmail.com>
2024-12-03 19:46:03 +0800

    feat: finish 2-1

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 1.388ms
+Time part 1: 1.121ms
 Time part 2: -
-Both parts: 1.388ms
+Both parts: 1.121ms
 ```
 
 ```
 Day 02
-Time part 1: -
+Time part 1: 5.575ms
 Time part 2: -
-Both parts: -
+Both parts: 5.575ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 1.388ms
+Total stars: 2/50
+Total time: 6.695ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.js
+++ src/day02/index.js
@@ -1,11 +1,39 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) => rawInput.split('\n');
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  console.log('input: ', input)
+  let sum = 0
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i].split(' ').map(Number)
+    let increase;
+    if (line[1] > line[0]) {
+      increase = true
+    }else {
+      increase = false
+    }
+    let safe = true
+    for (let j = 0; j < line.length; j++) {
+      if(line[j+1] <= line[j] && increase) {
+        safe = false
+        break
+      }
+      if(line[j+1] >= line[j] && !increase) {
+        safe = false
+        break
+      }
+      if(Math.abs(line[j+1] - line[j]) < 1 || Math.abs(line[j+1] - line[j]) > 3) {
+        safe = false
+        break
+      }
+    }
+    if (safe) {
+      sum += 1
+    }
+  }
+  return sum;
 };
 
 const part2 = (rawInput) => {
